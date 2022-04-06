93eab9553af5fa348c1db1682916aed787a1a491
Chao <362229416@qq.com>
2022-04-06 16:16:41 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/362228416/go-log
+
+go 1.17
+
+require (
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+)
---
+++ log/log.go
@@ -0,0 +1,60 @@
+package log
+
+import (
+	"github.com/natefinch/lumberjack"
+	"github.com/op/go-logging"
+	"io/ioutil"
+	"os"
+)
+
+var log = logging.MustGetLogger("go-log")
+
+func init() {
+	var format = logging.MustStringFormatter(
+		`%{time:2016/01/02 15:04:05} %{shortfile} [%{level:.5s}] %{message}`,
+	)
+	logging.SetFormatter(format)
+
+	logging.SetBackend(logging.NewLogBackend(&lumberjack.Logger{
+		Filename: GetLogsDir() + "/app.log",
+		MaxSize:  2,    // megabytes
+		Compress: true, // disabled by default
+	}, "", 0))
+}
+
+func Debug(args ...interface{}) {
+	log.Debug(args)
+}
+
+func Info(args ...interface{}) {
+	log.Info(args)
+}
+
+func Error(args ...interface{}) {
+	log.Error(args)
+}
+
+func Warning(args ...interface{}) {
+	log.Warning(args)
+}
+
+func GetLogsDir() string {
+	dir := "./logs"
+	found := false
+	for i := 0; i < 3; i++ {
+		fss, _ := ioutil.ReadDir(".")
+		for _, info := range fss {
+			if info.Name() == "logs" && info.IsDir() {
+				found = true
+				parentDir, _ := os.Getwd()
+				dir = parentDir + "/logs"
+				break
+			}
+		}
+		if !found {
+			os.Chdir("..")
+		}
+	}
+	log.Debug("Logs dir", dir)
+	return dir
+}
